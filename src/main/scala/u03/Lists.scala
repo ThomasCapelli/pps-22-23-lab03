e94f953f9d080218b95bafeb6210bd67664978c2
ThomasCapelli <thomas.capelli@studio.unibo.it>
2023-03-20 14:44:44 +0100

    es 1 and es 2 using tdd

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -1,5 +1,7 @@
 package u03
 
+import u02.Optionals.Option
+
 object Lists extends App :
 
   // A generic linkedlist
@@ -21,6 +23,23 @@ object Lists extends App :
       case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_, t) => filter(t)(pred)
       case Nil() => Nil()
+
+    def drop[A](l: List[A], n: Int): List[A] = l match
+      case Cons(_, t) if n > 1 => drop(t, n - 1)
+      case Cons(_, t) if n == 1 => t
+      case Nil() => Nil()
+    def append[A](left: List[A], right: List[A]): List[A] = left match
+      case Nil() => right;
+      case Cons(h, t) => Cons(h, append(t, right))
+    def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = list match
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+      case Nil() => Nil();
+
+    import u02.Optionals.Option.*
+    def max(l: List[Int]): Option[Int] = l match
+      case Cons(h, t) => max(t);
+      case Nil() => None();
+
 
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -20,3 +20,17 @@ class ListTest:
   @Test def testFilter() =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_>=20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_!=20))
+
+  @Test def testDrop() =
+    assertEquals(Cons(20, Cons(30, Nil())), drop(l, 1));
+    assertEquals(Cons(30, Nil()), drop(l, 2));
+  @Test def testAppend() =
+    assertEquals(Cons(10 , Cons(20, Cons(30, Cons(40, Nil())))), append(l, Cons(40, Nil())))
+  @Test def testFlatMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))), flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil()))))
+
+  import u02.Optionals.Option.*
+  @Test def testMax() =
+    assertEquals(Some(25), max(l))
+    assertEquals(None(), max(Nil()))
